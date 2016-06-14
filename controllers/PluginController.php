3250574701602c01b2e45359d856b32605cea098
shitikovkirill <sh.kiruh@gmail.com>
2016-06-14 08:37:24 +0300

    add dithub updater

--- controllers/PluginController.php
+++ controllers/PluginController.php
@@ -57,26 +57,23 @@ class PluginController extends Controller
 
         define('WP_GITHUB_FORCE_UPDATE', true);
 
-        if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+        $proper_folder_name = 'bunch-with-post';
+        $git_url = 'github.com/shitikovkirill/'.$proper_folder_name;
 
-            $proper_folder_name = 'superliv-checkout';
-            $git_url = 'github.com/shitikovkirill/'.$proper_folder_name;
+        $config = array(
+            'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+            'proper_folder_name' => $proper_folder_name, // this is the name of the folder your plugin lives in
+            'api_url' => 'https://api.'.$git_url, // the github API url of your github repo
+            'raw_url' => 'https://raw.'.$git_url.'/master', // the github raw url of your github repo
+            'github_url' => 'https://'.$git_url, // the github url of your github repo
+            'zip_url' => 'https://'.$git_url.'/zipball/master', // the zip url of the github repo
+            'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+            'requires' => '3.0', // which version of WordPress does your plugin require?
+            'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+            'readme' => 'README.md' // which file to use as the readme for the version number
+        );
 
-            $config = array(
-                'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-                'proper_folder_name' => $proper_folder_name, // this is the name of the folder your plugin lives in
-                'api_url' => 'https://api.'.$git_url, // the github API url of your github repo
-                'raw_url' => 'https://raw.'.$git_url.'/master', // the github raw url of your github repo
-                'github_url' => 'https://'.$git_url, // the github url of your github repo
-                'zip_url' => 'https://'.$git_url.'/zipball/master', // the zip url of the github repo
-                'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-                'requires' => '3.0', // which version of WordPress does your plugin require?
-                'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-                'readme' => 'README.md' // which file to use as the readme for the version number
-            );
-
-            $w = new WPGitHubUpdater($config);
-        }
+        $w = new WPGitHubUpdater($config);
 
     }
 }
