243c7e6e63c0b5ec76a4a05de9b4a49d5fbb8955
Shitikov Kirill <sh.kiruh@gmail.com>
2018-08-26 03:34:46 +0300

    add changes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -23,7 +23,6 @@ class DefaultController extends Controller
         return $this->render(
             'default/index.html.twig',
             [
-                'title' => 'Title',
                 'page' => $page,
             ]
         );
