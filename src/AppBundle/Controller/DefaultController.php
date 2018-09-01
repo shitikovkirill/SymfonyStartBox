9ae1078cf4cda9e9bf7d858cc3190286b169bda9
Shitikov Kirill <sh.kiruh@gmail.com>
2018-09-01 17:51:52 +0300

    Add OurWorks Category

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Header;
+use AppBundle\Entity\OurWorksCategory;
 use AppBundle\Entity\SecondSection;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -15,9 +16,24 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $ourWorksCategoryRep = $entityManager->getRepository(OurWorksCategory::class);
+        $ourWorksCategories = $ourWorksCategoryRep->findAll();
 
         return $this->render(
-            'default/index.html.twig'
+            'default/index.html.twig',
+            ['categories' => $ourWorksCategories]
+        );
+    }
+
+    /**
+     * @Route("our-work/{id}", name="our_work_item")
+     */
+    public function itemAction(Request $request, OurWorksCategory $category)
+    {
+        return $this->render(
+            'default/our-work.html.twig',
+            ['category' => $category]
         );
     }
 }
